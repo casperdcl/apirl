4692d38151a2cde7f8b01df3252518cf6db8392c
Casper da Costa-Luis <imaging@caspersci.uk.to>
2018-03-23 15:50:37 +0000

    dose update

--- matlab/andrew_reader_lab_software_interface/MLEM_all.m
+++ matlab/andrew_reader_lab_software_interface/MLEM_all.m
@@ -1,9 +1,8 @@
 %parpool('local', 6);
 i=0;
 for subj = [5, 20]
-for dose = [0.1, 1, 10, -1, 0.2, 0.4]
+for dose = [0.1, 1, 10, -1, .7, 0.2, 0.4]
 i = i + 1
-if i < 2, continue, end
 
 if subj==5, tumours=-1;
 else, tumours=-2;
@@ -12,6 +11,6 @@ end
 % negative counts -> no noise
 % negative tumours -> deterministic tumour generation
 reconAPIRL = MLEM(1, 1.5, 0.75, sprintf('subject_%02d', subj), ...
-                  500e6 * dose, tumours);
+                  430e6 * dose, tumours);
 
 end, end
