24e34d53cd8e3358f8b6b70a27f1f8c65ee908c0
Casper da Costa-Luis <imaging@caspersci.uk.to>
2018-04-12 18:48:44 +0100

    misc

--- matlab/andrew_reader_lab_software_interface/reground.m
+++ matlab/andrew_reader_lab_software_interface/reground.m
@@ -2,8 +2,9 @@
 tVersion = -1;
 PAT = num2str(-tVersion);
 %%
-fname = ['output/reconAPIRL_real-AD_' PAT '.mat'];
-uMap = permute(reshape(fread(fopen(['data-LM-00-umap-AD_' PAT '.v']), 'single'), [344 344 127]), [2 1 3]);
+rdir = 'output/';
+fname = [rdir 'reconAPIRL_real-AD_' PAT '.mat'];
+uMap = permute(reshape(fread(fopen([rdir 'data-LM-00-umap-AD_' PAT '.v']), 'single'), [344 344 127]), [2 1 3]);
 reconAPIRL = load(fname);
 reconAPIRL = reconAPIRL.reconAPIRL
 %%
@@ -18,8 +19,17 @@ wmm = size(im3d, 1)*reconAPIRL.voxelSize_mm(1);  % image width/[mm]
 im3d = permute(addTumours(permute(im3d, [3 2 1]), wmm, 5, tIntensity, tVersion), [3 2 1]);
 %}
 
+figure(1);
+[ha, pos] = tight_subplot(1,3,[.01 .01],[.01 .01],[.01 .01]);
+axes(ha(1)); imshow(1-sieves(110:end-120,120:end-120,64), []), title('(a)')
+axes(ha(2)), imshow(reconAPIRL.T1(110:end-120,120:end-120,64), []), title('(b)')
+axes(ha(3)), imshow(1-im3d(110:end-120,120:end-120,64), []), title('(c)')
+%set(ha(1:4),'XTickLabel',''); set(ha,'YTickLabel','')
+saveas(gcf, 'foo.png')
+input('continue? ');
+
 %%
-subplot(121), imshow(im3d(:,:,64), [])
+subplot(131), imshow(im3d(:,:,64), [])
 subplot(122), imshow(uMap(:,:,64), [])
 
 reconAPIRL.PET_psf_sharp = im3d;
